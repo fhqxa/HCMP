e18f0d995b26b67cba7c88112c4fdfd2f4065690
fhqxa <36149734+fhqxa@users.noreply.github.com>
2019-07-01 18:28:28 +0800

    Add files via upload

---
+++ function/TopkHierarchical/Kflod_TopKClassifier.m
@@ -0,0 +1,62 @@
+%% 10-fold
+%% Written by Hong Zhao
+%% Modified by Shunxin Guo
+% 2018-11-29
+%% Inputs:
+% data: all samples in the dataset
+% numFolds: fold the number
+% select_num: select the number of nodes
+% tree: the tree hierarchy
+%% Output
+function [accuracyMean,accuracyStd,F_LCAMean,FHMean,TIEMean,PredLabel,RealLabel] = Kflod_TopKClassifier(varargin)
+if(length(varargin) == 4)
+    data = varargin{1};
+    numFolds = varargin{2};
+    tree = varargin{3};
+    select_num = varargin{4};
+else
+    if(length(varargin) == 5)
+        data = [varargin{1},varargin{2}];
+        numFolds = varargin{3};
+        tree = varargin{4};
+        select_num = varargin{5};
+    end
+end
+[M,N]=size(data);
+accuracy_k = zeros(1,numFolds);
+rand('seed',1);
+indices = crossvalind('Kfold',data(1:M,N),numFolds);%Random subcontracting,K-fold cross-validation,k=10
+for k = 1:numFolds
+    testID = (indices == k);
+    trainID = ~testID;
+    test_data = data(testID,1:end-1);
+    test_label = data(testID,end);
+    train_data = data(trainID,:);
+    RealLabel{k} = test_label;
+    %% Creat sub table
+    [trainDataMod, trainLabelMod] = creatSubTable(train_data, tree); 
+    %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %% Train classifiers of all internal nodes
+    numNodes = length(tree(:,1));%ZH: The total of all nodes.
+    for i = 1:numNodes
+        if (~ismember(i, tree_LeafNode(tree)))
+            [model{i}] = train(double(sparse(trainLabelMod{i})), sparse(sparse(trainDataMod{i})), '-c 2 -s 0 -B 1 -q');
+        end
+    end    
+    %%           Prediction       %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    predict_label = TopKPrediction(test_data, model, tree,train_data,select_num) ;
+    PredLabel{k} = predict_label;
+    %%          Envaluation       %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+   [PH(k), RH(k), FH(k)] = EvaHier_HierarchicalPrecisionAndRecall(test_label,predict_label,tree);
+   [P_LCA(k),R_LCA(k),F_LCA(k)] = EvaHier_HierarchicalLCAPrecisionAndRecall(test_label,predict_label,tree);
+   TIE(k) = EvaHier_TreeInducedError(test_label,predict_label,tree);
+   accuracy_k(k) = EvaHier_HierarchicalAccuracy_gsx(test_label,predict_label, tree);%ÍõìÏ
+end
+    accuracyMean = mean(accuracy_k);
+	accuracyStd = std(accuracy_k);
+    F_LCAMean = mean(F_LCA);
+    FHMean = mean(FH);
+    TIEMean = mean(TIE);
+end
+
+    
---
+++ function/TopkHierarchical/TopKPrediction.m
@@ -0,0 +1,41 @@
+%% Top-K prediction
+% Written by Yu Wang 
+% Modified by Hong Zhao
+% Modified by Shunxin Guo
+% 2018-11-29
+%% Inputs:
+% input_data: testing data without labels
+% model: 
+% tree: the tree hierarchy
+% train_data: each fold of the training sample
+% k: select the number of nodes
+%% Output
+%  function [predict_label_KNN] = TopKPrediction(input_data, model,tree,train_data,k)
+function [predict_label_RF] = TopKPrediction(input_data, model, tree,train_data,k)    
+    [m,~]=size(input_data);
+    root = find(tree(:,1)==0);
+    for j=1:m %The number of instances
+%% Start at the root and select K nodes
+        [~,~,d_v] = predict(1,sparse(input_data(j,:)), model{root}, '-b 1 -q');
+        [n_d_v,IX]=sort(d_v,'descend');
+        q = max(tree(:,2)); %%Record the number of layers in the tree
+        for i =1:k
+            currentNodeID = IX(1,i);
+            mid_pro(i) = n_d_v(1,i);
+            currentNode(i) = model{root}.Label(currentNodeID) ;
+            currentNodeSelect(i,:)=[currentNode(i),mid_pro(i)];%
+        end
+%% Recursive middle level and select k nodes
+        for index = 1: q-1
+            currentNodeNew = [];
+            for i = 1:k %
+                currentNodeNew =[currentNodeNew; RecursiveLeaf(currentNodeSelect(i,:),input_data(j,:),model,tree,k)];
+            end
+            [n_d_v,IX]=sort(currentNodeNew(:,3),'descend');       
+            currentNodeSelect(1:k,:) = currentNodeNew(IX(1:k,1),1:2); 
+         end
+%% Call classifier      
+%         [predict_label_KNN(j,:)] = KNN(input_data(j,:),currentNodeSelect(:,1),train_data,k);
+      [predict_label_RF(j,:)] = RandomForest(input_data(j,:),currentNodeSelect(:,1),train_data,k);
+   end %%endfor    
+end
